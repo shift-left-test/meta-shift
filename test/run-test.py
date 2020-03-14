c0647adbef3e81bee27b68dd8076b7f9a322f3a1
Sung Gon Kim <sunggon82.kim@lge.com>
2020-03-14 12:43:13 +0000

    Adds pyro to the tests

--- test/run-test.py
+++ test/run-test.py
@@ -12,7 +12,7 @@ class YoctoProject(dict):
         self["sdk"] = self["env"].parse("core-image-minimal -c populate_sdk")
 
 
-@pytest.fixture(scope="module", params=["morty", "zeus"])
+@pytest.fixture(scope="module", params=["morty", "pyro", "zeus"])
 def yocto(request):
     return YoctoProject(request.param)
 
