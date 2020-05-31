4fe8375a2ece3ab839f12d769767cc06d96e6905
Sung Gon Kim <sunggon82.kim@lge.com>
2020-05-31 11:35:55 +0000

    #173 Rewrites mini-mcf.py

--- test/mini-mcf.py
+++ test/mini-mcf.py
@@ -14,22 +14,22 @@ import constants
 logging.basicConfig(level=logging.INFO, format="[ %(levelname)s ] %(message)s")
 logger = logging.getLogger(__name__)
 
-PWD = os.path.dirname(os.path.abspath(__file__))
 REPO_BASE_DIR = os.path.join(tempfile.gettempdir(), "meta-shift-repos-%s" % getpass.getuser())
 DEFAULT_BRANCH = constants.BRANCH
 DEFAULT_DIR = "build"
 
-repos = {
+REPOS = {
     "poky": { "url": "http://mod.lge.com/hub/yocto/mirror/poky.git", "location": "poky", "layer": "poky/meta" },
     "meta-oe": { "url": "http://mod.lge.com/hub/yocto/mirror/meta-openembedded.git", "location": "meta-openembedded", "layer": "meta-openembedded/meta-oe" },
     "meta-python": { "url": "http://mod.lge.com/hub/yocto/mirror/meta-openembedded.git", "location": "meta-openembedded", "layer": "meta-openembedded/meta-python" },
     "meta-qt5": { "url": "http://mod.lge.com/hub/yocto/mirror/meta-qt5.git", "location": "meta-qt5", "layer": "meta-qt5" },
+    "meta-shift": { "url": os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "location": "meta-shift", "layer": "meta-shift" },
     "meta-sample": { "url": "http://mod.lge.com/hub/yocto/sample/meta-sample.git", "location": "meta-sample", "layer": "meta-sample" },
     "meta-sample-test": { "url": "http://mod.lge.com/hub/yocto/sample/meta-sample-test.git", "location": "meta-sample-test", "layer": "meta-sample-test" },
 }
 
 
-def getopts():
+def parse_args():
     def exist_file(path):
         if not os.path.exists(path):
             raise argparse.ArgumentTypeError("File not exist: {}".format(path))
@@ -49,7 +49,7 @@ def execute(cmd):
     return subprocess.check_call(cmd, shell=True)
 
 
-def downloadRepo(url, branch, path):
+def download_repo(url, branch, path):
     if not os.path.exists(path):
         execute("git clone {} -b {} {}".format(url, branch, path))
     else:
@@ -57,58 +57,67 @@ def downloadRepo(url, branch, path):
         execute("git --git-dir={0}/.git --work-tree={0} pull".format(path))
 
 
-def downloadRepos(repodir, branch):
-    for name, repo in repos.items():
-        path = os.path.join(repodir, repo["location"])
+def download_repos(args):
+    logger.info("Downloading repos at {}...".format(args.repodir))
+    for name, repo in REPOS.items():
+        if name == "meta-shift":
+            continue
+        path = os.path.join(args.repodir, repo["location"])
         logger.info("REPO: {}".format(path))
-        downloadRepo(repo["url"], branch, path)
-
-def initBuildEnv(repodir, directory):
-    path = os.path.join(repodir, repos["poky"]["location"])
-    execute('bash -c "source {}/oe-init-build-env {}"'.format(path, directory))
+        download_repo(repo["url"], args.branch, path)
+    logger.info("Done")
 
 
-def configure(repodir, directory, filename):
-    bblayers_conf = os.path.join(directory, "conf/bblayers.conf")
-
-    with open(bblayers_conf, "a") as f:
-        f.write('BBLAYERS_append = " {}"\n'.format(os.path.abspath(os.path.dirname(PWD))))
-
+def read_json(filename):
     with open(filename, "r") as f:
-        conf = json.load(f)
+        return json.load(f)
 
+
+def cleanup_build_conf(args):
+    configures = [
+        os.path.join(args.directory, "conf", "bblayers.conf"),
+        os.path.join(args.directory, "conf", "local.conf")
+    ]
+    for configure in configures:
+        if os.path.exists(configure):
+            logger.warn("Removing the existing configuration file: {}".format(configure))
+            os.remove(configure)
+
+
+def configure_bblayers(args):
+    logger.info("Initializing bblayers.conf...")
+    commands = []
+    commands.append("source {}/{}/oe-init-build-env {}".format(args.repodir, REPOS["poky"]["location"], args.directory))
+    conf = read_json(args.filename)
     for include in conf["includes"]:
-        path = os.path.abspath(os.path.join(repodir, repos[include]["layer"]))
-        with open(bblayers_conf, "a") as f:
-            f.write('BBLAYERS_append = " {}"\n'.format(path))
+        path = os.path.join(args.repodir, REPOS[include]["layer"]) if include != "meta-shift" else REPOS[include]["url"]
+        commands.append("bitbake-layers add-layer {}".format(path))
+    execute('bash -c "{}"'.format("; ".join(commands)))
+    logger.info("Done")
 
-    local_conf = os.path.join(directory, "conf/local.conf")
+
+def configure_local(args):
+    logger.info("Initializing local.conf...")
+    local_conf = os.path.join(args.directory, "conf", "local.conf")
+    conf = read_json(args.filename)
     for key, value in conf["local.conf"].items():
         with open(local_conf, "a") as f:
-            f.write('{} = "{}"\n'.format(key, value.replace("${HOME}", os.path.expanduser("~"))))
+            f.write('{} = "{}"\n'.format(key, value))
+    logger.info("Done")
 
 
-def printUsage(repodir, directory):
+def print_usage(args):
     print("""\n\n\n### Shell environment set up command ###
 
     source {0}/poky/oe-init-build-env {1}
 
-    """.format(repodir, directory))
+    """.format(args.repodir, args.directory))
 
 
 if __name__ == "__main__":
-    options = getopts()
-
-    logger.info("Downloading repos at {0}...".format(options.repodir))
-    downloadRepos(options.repodir, options.branch)
-    logger.info("Done.")
-
-    logger.info("Initialzing build environment...")
-    initBuildEnv(options.repodir, options.directory)
-    logger.info("Done.")
-
-    logger.info("Configuring files...")
-    configure(options.repodir, options.directory, options.filename)
-    logger.info("Done.")
-
-    printUsage(options.repodir, options.directory)
+    args = parse_args()
+    download_repos(args)
+    cleanup_build_conf(args)
+    configure_bblayers(args)
+    configure_local(args)
+    print_usage(args)
