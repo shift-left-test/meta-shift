d1b30089b42ab98bed6428ee4d20e4f0e86f58c4
Daeseong Seong <daeseong.seong@lgepartner.com>
2020-06-04 06:29:53 +0000

    fix version issue from morty to pyro

--- test/TestTest.py
+++ test/TestTest.py
@@ -22,7 +22,6 @@ class core_image_minimal(unittest.TestCase):
                                  "libsqlite3wrapper0-0.1.0-r0.aarch64.rpm")
 
         assert self.build.files.exists("buildhistory/packages/aarch64-poky-linux/gtest/latest")
-        assert self.build.files.exists("buildhistory/packages/aarch64-poky-linux/gmock/latest")
 
     def test_do_test(self):
         o = self.build.shell.execute("bitbake core-image-minimal -c test")
