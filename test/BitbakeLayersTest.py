fd4c509c247d4fbd5062f55f845a91dced9a6042
Sung Gon Kim <sunggon82.kim@lge.com>
2020-06-07 12:11:23 +0000

    #192 Adds bitbake-layers test-recipes subcommand for morty

--- test/BitbakeLayersTest.py
+++ test/BitbakeLayersTest.py
@@ -28,6 +28,37 @@ class TestLayersWithRelease(unittest.TestCase):
         o = self.build.shell.execute("bitbake-layers test-layers --show --basepath /dev/null")
         assert not o.stdout.contains("meta-sample-test")
 
+    def test_add_remove(self):
+        assert not self.build.shell.execute("bitbake-layers show-layers").stdout.contains("meta-sample-test")
+        self.build.shell.execute("bitbake-layers test-layers --add")
+        assert self.build.shell.execute("bitbake-layers show-layers").stdout.contains("meta-sample-test")
+        self.build.shell.execute("bitbake-layers test-layers --remove")
+        assert not self.build.shell.execute("bitbake-layers show-layers").stdout.contains("meta-sample-test")
+
+
+class TestRecipesWithTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.build = yocto.BuildEnvironment(branch=constants.BRANCH, conf=constants.TEST)
+
+    def test_show_recipes(self):
+        o = self.build.shell.execute("bitbake-layers test-recipes")
+        assert o.stdout.containsAll("autotools-project              1.0.0                meta-sample",
+                                    "cmake-project                  1.0.0                meta-sample",
+                                    "humidifier-project             1.0.0                meta-sample",
+                                    "qmake5-project                 1.0.0                meta-sample",
+                                    "sqlite3wrapper                 0.1.0                meta-sample",
+                                    "stringutils                    0.0.1                meta-sample")
+
+    def test_show_recipes_with_pnspec(self):
+        o = self.build.shell.execute("bitbake-layers test-recipes *-project")
+        assert o.stdout.containsAll("autotools-project              1.0.0                meta-sample",
+                                    "cmake-project                  1.0.0                meta-sample",
+                                    "humidifier-project             1.0.0                meta-sample",
+                                    "qmake5-project                 1.0.0                meta-sample")
+        assert not o.stdout.containsAll("sqlite3wrapper                 0.1.0                meta-sample",
+                                        "stringutils                    0.0.1                meta-sample")
+
 
 if __name__ == "__main__":
     pytest.main(["-v", __file__])
