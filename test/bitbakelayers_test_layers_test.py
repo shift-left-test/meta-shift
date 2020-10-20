01fe2bdd1018b7d194e28971d0006b17de8145a0
Sung Gon Kim <sunggon82.kim@lge.com>
2020-10-20 10:10:05 +0900

    #344 Fixes irregular build failures due to the mixed fixtures

--- test/bitbakelayers_test_layers_test.py
+++ test/bitbakelayers_test_layers_test.py
@@ -57,19 +57,20 @@ def test_add_layers(release_build):
     assert not release_build.shell.execute("bitbake-layers show-layers").stdout.contains("meta-sample-test")
 
 
-def test_add_layers_twice(test_build):
-    assert test_build.shell.execute("bitbake-layers show-layers").stdout.contains("meta-sample-test")
-    o = test_build.shell.execute("bitbake-layers test-layers --add")
+def test_add_layers_twice(release_build):
+    release_build.shell.execute("bitbake-layers test-layers --add")
+    assert release_build.shell.execute("bitbake-layers show-layers").stdout.contains("meta-sample-test")
+    o = release_build.shell.execute("bitbake-layers test-layers --add")
     regexp = re.compile("Specified layer .+/meta-sample-test is already in BBLAYERS", re.MULTILINE)
     assert bool(re.match(regexp, o.stderr.output))
+    release_build.shell.execute("bitbake-layers test-layers --remove")
 
 
-def test_remove_layers(test_build):
-    assert test_build.shell.execute("bitbake-layers show-layers").stdout.contains("meta-sample-test")
-    test_build.shell.execute("bitbake-layers test-layers --remove")
-    assert not test_build.shell.execute("bitbake-layers show-layers").stdout.contains("meta-sample-test")
-    test_build.shell.execute("bitbake-layers test-layers --add")
-    assert test_build.shell.execute("bitbake-layers show-layers").stdout.contains("meta-sample-test")
+def test_remove_layers(release_build):
+    release_build.shell.execute("bitbake-layers test-layers --add")
+    assert release_build.shell.execute("bitbake-layers show-layers").stdout.contains("meta-sample-test")
+    release_build.shell.execute("bitbake-layers test-layers --remove")
+    assert not release_build.shell.execute("bitbake-layers show-layers").stdout.contains("meta-sample-test")
 
 
 def test_remove_layers_twice(release_build):
