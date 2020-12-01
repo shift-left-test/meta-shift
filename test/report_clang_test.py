b98137b8a28b8cfd9e34493db7953a962c36dbee
Daeseong Seong <daeseong.seong@lgepartner.com>
2020-12-01 04:18:46 +0000

    add test code for checktest

--- test/report_clang_test.py
+++ test/report_clang_test.py
@@ -37,6 +37,7 @@ QT_PLUS_TEST_FAILED_LOG = 'testsuite errors="0" failures="1" tests="4" name="qma
 QT_MINUS_TEST_FAILED_LOG = 'testsuite errors="0" failures="1" tests="4" name="qmake5-project.MinusTest"'
 CPPCHECK_NO_ERRORS_FOUND = '<cppcheck version="2.0"/>\n    <errors>\n    </errors>'
 LCOV_HTML_TITLE = '<tr><td class="title">LCOV - code coverage report</td></tr>'
+SENTINEL_HTML_TITLE = '<h1>Sentinel Mutation Coverage Report</h1>'
 
 
 class TEST:
@@ -60,6 +61,10 @@ class TEST:
     @classmethod
     def CHECK(cls, recipe, path):
         return os.path.join("report", cls.PF[recipe], "checkcode", path)
+
+    @classmethod
+    def CHECKTEST(cls, recipe, path):
+        return os.path.join("report", cls.PF[recipe], "checktest", path)
 
 
 def test_core_image_minimal_do_coverageall(report_clang_build):
@@ -128,6 +133,42 @@ def test_core_image_minimal_do_checkcodeall(report_clang_build):
     assert EXISTS(TEST.CHECK("stringutils", "clang-tidy-report.txt"))
 
 
+def test_core_image_minimal_do_checktestall(report_clang_build):
+    report_clang_build.files.remove("report")
+
+    assert report_clang_build.shell.execute("bitbake core-image-minimal -c checktestall").stderr.empty()
+
+    EXISTS = report_clang_build.files.exists
+
+    assert EXISTS(TEST.CHECKTEST("cmake-project", "mutations.xml"))
+    assert EXISTS(TEST.CHECKTEST("cmake-project", "index.xml"))
+    assert EXISTS(TEST.CHECKTEST("cmake-project", "style.css"))
+
+    assert EXISTS(TEST.CHECKTEST("qmake5-project", "mutations.xml"))
+    assert EXISTS(TEST.CHECKTEST("qmake5-project", "index.xml"))
+    assert EXISTS(TEST.CHECKTEST("qmake5-project", "style.css"))
+
+    assert EXISTS(TEST.CHECKTEST("autotools-project", "mutations.xml"))
+    assert EXISTS(TEST.CHECKTEST("autotools-project", "index.xml"))
+    assert EXISTS(TEST.CHECKTEST("autotools-project", "style.css"))
+
+    assert EXISTS(TEST.CHECKTEST("humidifier-project", "mutations.xml"))
+    assert EXISTS(TEST.CHECKTEST("humidifier-project", "index.xml"))
+    assert EXISTS(TEST.CHECKTEST("humidifier-project", "style.css"))
+
+    assert EXISTS(TEST.CHECKTEST("sqlite3wrapper", "mutations.xml"))
+    assert EXISTS(TEST.CHECKTEST("sqlite3wrapper", "index.xml"))
+    assert EXISTS(TEST.CHECKTEST("sqlite3wrapper", "style.css"))
+
+    assert EXISTS(TEST.CHECKTEST("sqlite3wrapper", "mutations.xml"))
+    assert EXISTS(TEST.CHECKTEST("sqlite3wrapper", "index.xml"))
+    assert EXISTS(TEST.CHECKTEST("sqlite3wrapper", "style.css"))
+
+    assert EXISTS(TEST.CHECKTEST("stringutils", "mutations.xml"))
+    assert EXISTS(TEST.CHECKTEST("stringutils", "index.xml"))
+    assert EXISTS(TEST.CHECKTEST("stringutils", "style.css"))
+
+
 def test_cmake_project_do_coverageall(report_clang_build):
     report_clang_build.files.remove("report")
 
@@ -159,6 +200,16 @@ def test_cmake_project_do_checkcodeall(report_clang_build):
     # assert READ(TEST.CHECK("cmake-project", "cppcheck_report.xml")).contains(CPPCHECK_NO_ERRORS_FOUND)
     assert READ(TEST.CHECK("cmake-project", "cpplint_report.txt")).empty()
     assert READ(TEST.CHECK("cmake-project", "clang-tidy-report.txt")).empty()
+
+
+def test_cmake_project_do_checktestall(report_clang_build):
+    report_clang_build.files.remove("report")
+    assert report_clang_build.shell.execute("bitbake cmake-project -c checktestall").stderr.empty()
+    READ = report_clang_build.files.read
+    with READ(TEST.CHECKTEST("cmake-project", "mutations.xml")) as f:
+        assert f.contains('<mutations/>')
+    with READ(TEST.CHECKTEST("cmake-project", "index.html")) as f:
+        assert f.contains(SENTINEL_HTML_TITLE)
 
 
 def test_qmake5_project_do_coverageall(report_clang_build):
@@ -201,6 +252,16 @@ def test_qmake5_project_do_checkcodeall(report_clang_build):
     # assert not READ(TEST.CHECK("qmake5-project", "clang-tidy-report.txt")).empty()
 
 
+def test_qmake5_project_do_checktestall(report_clang_build):
+    report_clang_build.files.remove("report")
+    assert report_clang_build.shell.execute("bitbake qmake5-project -c checktestall").stderr.empty()
+    READ = report_clang_build.files.read
+    with READ(TEST.CHECKTEST("qmake5-project", "mutations.xml")) as f:
+        assert f.contains('<mutations/>')
+    with READ(TEST.CHECKTEST("qmake5-project", "index.html")) as f:
+        assert f.contains(SENTINEL_HTML_TITLE)
+
+
 def test_autotools_project_do_coverageall(report_clang_build):
     report_clang_build.files.remove("report")
 
@@ -232,6 +293,16 @@ def test_autotools_project_do_checkcodeall(report_clang_build):
     assert READ(TEST.CHECK("autotools-project", "clang-tidy-report.txt")).empty()
 
 
+def test_autotools_project_do_checktestall(report_clang_build):
+    report_clang_build.files.remove("report")
+    assert report_clang_build.shell.execute("bitbake autotools-project -c checktestall").stderr.empty()
+    READ = report_clang_build.files.read
+    with READ(TEST.CHECKTEST("autotools-project", "mutations.xml")) as f:
+        assert f.contains('<mutations/>')
+    with READ(TEST.CHECKTEST("autotools-project", "index.html")) as f:
+        assert f.contains(SENTINEL_HTML_TITLE)
+
+
 def test_humidifier_project_do_coverageall(report_clang_build):
     report_clang_build.files.remove("report")
 
@@ -258,6 +329,16 @@ def test_humidifier_project_do_checkcodeall(report_clang_build):
     # assert READ(TEST.CHECK("humidifier-project", "cppcheck_report.xml")).contains(CPPCHECK_NO_ERRORS_FOUND)
     assert READ(TEST.CHECK("humidifier-project", "cpplint_report.txt")).empty()
     assert READ(TEST.CHECK("humidifier-project", "clang-tidy-report.txt")).empty()
+
+
+def test_humidifier_project_do_checktestall(report_clang_build):
+    report_clang_build.files.remove("report")
+    assert report_clang_build.shell.execute("bitbake humidifier-project -c checktestall").stderr.empty()
+    READ = report_clang_build.files.read
+    with READ(TEST.CHECKTEST("humidifier-project", "mutations.xml")) as f:
+        assert f.contains('<mutations/>')
+    with READ(TEST.CHECKTEST("humidifier-project", "index.html")) as f:
+        assert f.contains(SENTINEL_HTML_TITLE)
 
 
 def test_sqlite3logger_do_coverageall(report_clang_build):
@@ -296,3 +377,17 @@ def test_sqlite3logger_do_checkcodeall(report_clang_build):
     # assert READ(TEST.CHECK("stringutils", "cppcheck_report.xml")).contains(CPPCHECK_NO_ERRORS_FOUND)
     assert READ(TEST.CHECK("stringutils", "cpplint_report.txt")).empty()
     assert READ(TEST.CHECK("stringutils", "clang-tidy-report.txt")).empty()
+
+
+def test_sqlite3logger_project_do_checktestall(report_clang_build):
+    report_clang_build.files.remove("report")
+    assert report_clang_build.shell.execute("bitbake sqlite3logger -c checktestall").stderr.empty()
+    READ = report_clang_build.files.read
+    with READ(TEST.CHECKTEST("sqlite3wrapper", "mutations.xml")) as f:
+        assert f.contains('<mutations/>')
+    with READ(TEST.CHECKTEST("sqlite3wrapper", "index.html")) as f:
+        assert f.contains(SENTINEL_HTML_TITLE)
+    with READ(TEST.CHECKTEST("stringutils", "mutations.xml")) as f:
+        assert f.contains('<mutations/>')
+    with READ(TEST.CHECKTEST("stringutils", "index.html")) as f:
+        assert f.contains(SENTINEL_HTML_TITLE)
--- test/test_clang_test.py
+++ test/test_clang_test.py
@@ -97,6 +97,27 @@ def test_core_image_minimal_do_checkcodeall(test_clang_build):
     assert o.stdout.contains("stringutils-0.0.1-r0 do_checkcode: INFO:SAGE:* clang-tidy is running...")
 
 
+def test_core_image_minimal_do_checktest(test_clang_build):
+    o = test_clang_build.shell.execute("bitbake core-image-minimal -c checktest")
+    assert o.stderr.contains("ERROR: Task do_checktest does not exist for target core-image-minimal")
+
+
+def test_core_image_minimal_do_checktestall(test_clang_build):
+    o = test_clang_build.shell.execute("bitbake core-image-minimal -c checktestall")
+    assert o.stdout.contains("cmake-project-1.0.0-r0 do_checktest:                    Mutant Population Report")
+    assert o.stdout.contains("cmake-project-1.0.0-r0 do_checktest:                              Mutation Coverage Report")
+    assert o.stdout.contains("humidifier-project-1.0.0-r0 do_checktest:                    Mutant Population Report")
+    assert o.stdout.contains("humidifier-project-1.0.0-r0 do_checktest:                              Mutation Coverage Report")
+    assert o.stdout.contains("sqlite3wrapper-0.1.0-r0 do_checktest:                    Mutant Population Report")
+    assert o.stdout.contains("sqlite3wrapper-0.1.0-r0 do_checktest:                              Mutation Coverage Report")
+    assert o.stdout.contains("qmake5-project-1.0.0-r0 do_checktest:                    Mutant Population Report")
+    assert o.stdout.contains("qmake5-project-1.0.0-r0 do_checktest:                              Mutation Coverage Report")
+    assert o.stdout.contains("autotools-project-1.0.0-r0 do_checktest:                    Mutant Population Report")
+    assert o.stdout.contains("autotools-project-1.0.0-r0 do_checktest:                              Mutation Coverage Report")
+    assert o.stdout.contains("stringutils-0.0.1-r0 do_checktest:                    Mutant Population Report")
+    assert o.stdout.contains("stringutils-0.0.1-r0 do_checktest:                              Mutation Coverage Report")
+
+
 def test_cmake_project_do_build(test_clang_build):
     assert test_clang_build.shell.execute("bitbake cmake-project").stderr.empty()
 
@@ -149,6 +170,18 @@ def test_cmake_project_do_checkcodeall(test_clang_build):
     assert o.stdout.contains("cmake-project-1.0.0-r0 do_checkcode: INFO:SAGE:* cppcheck is running...")
     assert o.stdout.contains("cmake-project-1.0.0-r0 do_checkcode: INFO:SAGE:* cpplint is running...")
     assert o.stdout.contains("cmake-project-1.0.0-r0 do_checkcode: INFO:SAGE:* clang-tidy is running...")
+
+
+def test_cmake_project_do_checktest(test_clang_build):
+    o = test_clang_build.shell.execute("bitbake cmake-project -c checktest")
+    assert o.stdout.contains("cmake-project-1.0.0-r0 do_checktest:                    Mutant Population Report")
+    assert o.stdout.contains("cmake-project-1.0.0-r0 do_checktest:                              Mutation Coverage Report")
+
+
+def test_cmake_project_do_checktestall(test_clang_build):
+    o = test_clang_build.shell.execute("bitbake cmake-project -c checktestall")
+    assert o.stdout.contains("cmake-project-1.0.0-r0 do_checktest:                    Mutant Population Report")
+    assert o.stdout.contains("cmake-project-1.0.0-r0 do_checktest:                              Mutation Coverage Report")
 
 
 def test_qmake5_project_do_build(test_clang_build):
@@ -209,6 +242,18 @@ def test_qmake5_project_do_checkcodeall(test_clang_build):
     assert o.stdout.contains("qmake5-project-1.0.0-r0 do_checkcode: INFO:SAGE:* clang-tidy is running...")
 
 
+def test_qmake5_project_do_checktest(test_clang_build):
+    o = test_clang_build.shell.execute("bitbake qmake5-project -c checktest")
+    assert o.stdout.contains("qmake5-project-1.0.0-r0 do_checktest:                    Mutant Population Report")
+    assert o.stdout.contains("qmake5-project-1.0.0-r0 do_checktest:                              Mutation Coverage Report")
+
+
+def test_qmake5_project_do_checktestall(test_clang_build):
+    o = test_clang_build.shell.execute("bitbake qmake5-project -c checktestall")
+    assert o.stdout.contains("qmake5-project-1.0.0-r0 do_checktest:                    Mutant Population Report")
+    assert o.stdout.contains("qmake5-project-1.0.0-r0 do_checktest:                              Mutation Coverage Report")
+
+
 def test_autotools_project_do_build(test_clang_build):
     assert test_clang_build.shell.execute("bitbake autotools-project").stderr.empty()
 
@@ -260,6 +305,18 @@ def test_autotools_project_do_checkcodeall(test_clang_build):
     assert o.stdout.contains("autotools-project-1.0.0-r0 do_checkcode: INFO:SAGE:* cppcheck is running...")
     assert o.stdout.contains("autotools-project-1.0.0-r0 do_checkcode: INFO:SAGE:* cpplint is running...")
     assert o.stdout.contains("autotools-project-1.0.0-r0 do_checkcode: INFO:SAGE:* clang-tidy is running...")
+
+
+def test_autotools_project_do_checktest(test_clang_build):
+    o = test_clang_build.shell.execute("bitbake autotools-project -c checktest")
+    assert o.stdout.contains("autotools-project-1.0.0-r0 do_checktest:                    Mutant Population Report")
+    assert o.stdout.contains("autotools-project-1.0.0-r0 do_checktest:                              Mutation Coverage Report")
+
+
+def test_autotools_project_do_checktestall(test_clang_build):
+    o = test_clang_build.shell.execute("bitbake autotools-project -c checktestall")
+    assert o.stdout.contains("autotools-project-1.0.0-r0 do_checktest:                    Mutant Population Report")
+    assert o.stdout.contains("autotools-project-1.0.0-r0 do_checktest:                              Mutation Coverage Report")
 
 
 def test_humidifier_project_do_build(test_clang_build):
@@ -314,6 +371,18 @@ def test_humidifier_project_do_checkcodeall(test_clang_build):
     assert o.stdout.contains("humidifier-project-1.0.0-r0 do_checkcode: INFO:SAGE:* cppcheck is running...")
     assert o.stdout.contains("humidifier-project-1.0.0-r0 do_checkcode: INFO:SAGE:* cpplint is running...")
     assert o.stdout.contains("humidifier-project-1.0.0-r0 do_checkcode: INFO:SAGE:* clang-tidy is running...")
+
+
+def test_humidifier_project_do_checktest(test_clang_build):
+    o = test_clang_build.shell.execute("bitbake humidifier-project -c checktest")
+    assert o.stdout.contains("humidifier-project-1.0.0-r0 do_checktest:                    Mutant Population Report")
+    assert o.stdout.contains("humidifier-project-1.0.0-r0 do_checktest:                              Mutation Coverage Report")
+
+
+def test_humidifier_project_do_checktestall(test_clang_build):
+    o = test_clang_build.shell.execute("bitbake humidifier-project -c checktestall")
+    assert o.stdout.contains("humidifier-project-1.0.0-r0 do_checktest:                    Mutant Population Report")
+    assert o.stdout.contains("humidifier-project-1.0.0-r0 do_checktest:                              Mutation Coverage Report")
 
 
 def test_sqlite3logger_do_build(test_clang_build):
@@ -374,3 +443,16 @@ def test_sqlite3logger_do_checkcodeall(test_clang_build):
     assert o.stdout.contains("sqlite3wrapper-0.1.0-r0 do_checkcode: INFO:SAGE:* cppcheck is running...")
     assert o.stdout.contains("sqlite3wrapper-0.1.0-r0 do_checkcode: INFO:SAGE:* cpplint is running...")
     assert o.stdout.contains("sqlite3wrapper-0.1.0-r0 do_checkcode: INFO:SAGE:* clang-tidy is running...")
+
+
+def test_sqlite3logger_project_do_checktest(test_clang_build):
+    o = test_clang_build.shell.execute("bitbake sqlite3logger -c checktest")
+    assert o.stderr.contains("ERROR: Task do_checktest does not exist for target sqlite3logger")
+
+
+def test_sqlite3logger_project_do_checktestall(test_clang_build):
+    o = test_clang_build.shell.execute("bitbake sqlite3logger -c checktestall")
+    assert o.stdout.contains("sqlite3wrapper-0.1.0-r0 do_checktest:                    Mutant Population Report")
+    assert o.stdout.contains("sqlite3wrapper-0.1.0-r0 do_checktest:                              Mutation Coverage Report")
+    assert o.stdout.contains("stringutils-0.0.1-r0 do_checktest:                    Mutant Population Report")
+    assert o.stdout.contains("stringutils-0.0.1-r0 do_checktest:                              Mutation Coverage Report")
