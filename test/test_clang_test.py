9098ab353bc28a5ed6034e4840fff9ec4183c8a2
sangmo.kang <sangmo.kang@lge.com>
2021-02-06 02:11:55 +0900

    #387 add CHECKTEST_SEED option and improve CHECKTEST_VERBOSE option

--- test/test_clang_test.py
+++ test/test_clang_test.py
@@ -98,3 +98,21 @@ def test_autotools_project_do_checktest(test_clang_build):
     o = test_clang_build.shell.execute("bitbake autotools-project -c checktest")
     assert o.stdout.contains("autotools-project-1.0.0-r0 do_checktest:                    Mutant Population Report")
     assert o.stdout.contains("autotools-project-1.0.0-r0 do_checktest:                              Mutation Coverage Report")
+
+
+def test_do_checktest_seed_option(test_clang_build):
+    o = test_clang_build.shell.execute("bitbake cmake-project -c checktest")
+    assert o.stdout.contains("cmake-project/1.0.0-r0/git/plus/src/plus.cpp,plus,30,12,30,13,*")
+    assert o.stdout.contains("cmake-project/1.0.0-r0/git/program/main.cpp,main,37,39,37,40,*")
+
+    o = test_clang_build.shell.execute("bitbake cmake-project -c checktestall")
+    assert o.stdout.contains("cmake-project/1.0.0-r0/git/plus/src/plus.cpp,plus,30,12,30,13,*")
+    assert o.stdout.contains("cmake-project/1.0.0-r0/git/program/main.cpp,main,37,39,37,40,*")
+
+
+def test_do_checktest_verbose_option(test_clang_build):
+    o = test_clang_build.shell.execute("bitbake autotools-project -c checktest")
+    assert o.stdout.contains("autotools-project-1.0.0-r0 do_checktest: CommandPopulate [INFO] random seed:1234")
+    assert o.stdout.contains("autotools-project-1.0.0-r0 do_checktest: CommandMutate [INFO] mutant:")
+    assert o.stdout.contains("autotools-project-1.0.0-r0 do_checktest: CommandEvaluate [INFO] mutant:")
+    assert o.stdout.contains("autotools-project-1.0.0-r0 do_checktest: CommandReport [INFO] evaluation-file")
