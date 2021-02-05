8b380c526097ddfd4e474960702b8a15044c1fb8
Sung Gon Kim <sunggon82.kim@lge.com>
2021-02-06 00:35:24 +0900

    #364 Removes redundant clang test cases to reduce the test time

--- test/report_clang_test.py
+++ test/report_clang_test.py
@@ -45,18 +45,7 @@ class TEST:
         "cmake-project": "cmake-project-1.0.0-r0",
         "qmake5-project": "qmake5-project-1.0.0-r0",
         "autotools-project": "autotools-project-1.0.0-r0",
-        "humidifier-project": "humidifier-project-1.0.0-r0",
-        "sqlite3wrapper": "sqlite3wrapper-0.1.0-r0",
-        "stringutils": "stringutils-0.0.1-r0",
     }
-
-    @classmethod
-    def RESULT(cls, recipe, path):
-        return os.path.join("report", cls.PF[recipe], "test", path)
-
-    @classmethod
-    def COVERAGE(cls, recipe, path):
-        return os.path.join("report", cls.PF[recipe], "coverage", path)
 
     @classmethod
     def CHECK(cls, recipe, path):
@@ -65,40 +54,6 @@ class TEST:
     @classmethod
     def CHECKTEST(cls, recipe, path):
         return os.path.join("report", cls.PF[recipe], "checktest", path)
-
-
-def test_core_image_minimal_do_coverageall(report_clang_build):
-    report_clang_build.files.remove("report")
-
-    assert report_clang_build.shell.execute("bitbake core-image-minimal -c coverageall").stderr.empty()
-
-    EXISTS = report_clang_build.files.exists
-
-    assert EXISTS(TEST.RESULT("cmake-project", "OperatorTest.xml"))
-    assert EXISTS(TEST.COVERAGE("cmake-project", "index.html"))
-    assert EXISTS(TEST.COVERAGE("cmake-project", "coverage.xml"))
-
-    assert EXISTS(TEST.RESULT("qmake5-project", "test-qt5-gtest.xml"))
-    assert EXISTS(TEST.RESULT("qmake5-project", "tests/plus_test/test_result.xml"))
-    assert EXISTS(TEST.RESULT("qmake5-project", "tests/minus_test/test_result.xml"))
-    assert EXISTS(TEST.COVERAGE("qmake5-project", "index.html"))
-    assert EXISTS(TEST.COVERAGE("qmake5-project", "coverage.xml"))
-
-    assert EXISTS(TEST.RESULT("autotools-project", "operatorTest.xml"))
-    assert EXISTS(TEST.COVERAGE("autotools-project", "index.html"))
-    assert EXISTS(TEST.COVERAGE("autotools-project", "coverage.xml"))
-
-    assert EXISTS(TEST.RESULT("humidifier-project", "unittest.xml"))
-    assert EXISTS(TEST.COVERAGE("humidifier-project", "index.html"))
-    assert EXISTS(TEST.COVERAGE("humidifier-project", "coverage.xml"))
-
-    assert EXISTS(TEST.RESULT("sqlite3wrapper", "SQLite3WrapperTest.exe.xml"))
-    assert EXISTS(TEST.COVERAGE("sqlite3wrapper", "index.html"))
-    assert EXISTS(TEST.COVERAGE("sqlite3wrapper", "coverage.xml"))
-
-    assert EXISTS(TEST.RESULT("stringutils", "unittest.bin.xml"))
-    assert EXISTS(TEST.COVERAGE("stringutils", "index.html"))
-    assert EXISTS(TEST.COVERAGE("stringutils", "coverage.xml"))
 
 
 def test_core_image_minimal_do_checkcodeall(report_clang_build):
@@ -120,18 +75,6 @@ def test_core_image_minimal_do_checkcodeall(report_clang_build):
     assert EXISTS(TEST.CHECK("autotools-project", "cpplint_report.txt"))
     assert EXISTS(TEST.CHECK("autotools-project", "clang-tidy-report.txt"))
 
-    assert EXISTS(TEST.CHECK("humidifier-project", "cppcheck_report.xml"))
-    assert EXISTS(TEST.CHECK("humidifier-project", "cpplint_report.txt"))
-    assert EXISTS(TEST.CHECK("humidifier-project", "clang-tidy-report.txt"))
-
-    assert EXISTS(TEST.CHECK("sqlite3wrapper", "cppcheck_report.xml"))
-    assert EXISTS(TEST.CHECK("sqlite3wrapper", "cpplint_report.txt"))
-    assert EXISTS(TEST.CHECK("sqlite3wrapper", "clang-tidy-report.txt"))
-
-    assert EXISTS(TEST.CHECK("stringutils", "cppcheck_report.xml"))
-    assert EXISTS(TEST.CHECK("stringutils", "cpplint_report.txt"))
-    assert EXISTS(TEST.CHECK("stringutils", "clang-tidy-report.txt"))
-
 
 def test_core_image_minimal_do_checktestall(report_clang_build):
     report_clang_build.files.remove("report")
@@ -152,42 +95,6 @@ def test_core_image_minimal_do_checktestall(report_clang_build):
     assert EXISTS(TEST.CHECKTEST("autotools-project", "index.html"))
     assert EXISTS(TEST.CHECKTEST("autotools-project", "style.css"))
 
-    assert EXISTS(TEST.CHECKTEST("humidifier-project", "mutations.xml"))
-    assert EXISTS(TEST.CHECKTEST("humidifier-project", "index.html"))
-    assert EXISTS(TEST.CHECKTEST("humidifier-project", "style.css"))
-
-    assert EXISTS(TEST.CHECKTEST("sqlite3wrapper", "mutations.xml"))
-    assert EXISTS(TEST.CHECKTEST("sqlite3wrapper", "index.html"))
-    assert EXISTS(TEST.CHECKTEST("sqlite3wrapper", "style.css"))
-
-    assert EXISTS(TEST.CHECKTEST("stringutils", "mutations.xml"))
-    assert EXISTS(TEST.CHECKTEST("stringutils", "index.html"))
-    assert EXISTS(TEST.CHECKTEST("stringutils", "style.css"))
-
-
-def test_cmake_project_do_coverageall(report_clang_build):
-    report_clang_build.files.remove("report")
-
-    assert report_clang_build.shell.execute("bitbake cmake-project -c coverageall").stderr.empty()
-
-    READ = report_clang_build.files.read
-
-    with READ(TEST.RESULT("cmake-project", "OperatorTest_1.xml")) as f:
-        assert f.contains('classname="cmake-project.PlusTest"')
-        assert f.contains(CMAKE_GT_PLUS_TEST_FAILED_LOG)
-
-    with READ(TEST.RESULT("cmake-project", "OperatorTest_3.xml")) as f:
-        assert f.contains('classname="cmake-project.MinusTest"')
-        assert f.contains(CMAKE_GT_MINUS_TEST_FAILED_LOG)
-
-    assert READ(TEST.COVERAGE("cmake-project", "index.html")).contains(LCOV_HTML_TITLE)
-
-    with READ(TEST.COVERAGE("cmake-project", "coverage.xml")) as f:
-        assert f.contains('name="cmake-project.plus.src"')
-        assert f.contains('<method name="arithmetic::plus(int, int)" signature="" line-rate="1.0" branch-rate="1.0">')
-        assert f.contains('name="cmake-project.minus.src"')
-        assert f.contains('<method name="arithmetic::minus(int, int)" signature="" line-rate="1.0" branch-rate="1.0">')
-
 
 def test_cmake_project_do_checkcodeall(report_clang_build):
     report_clang_build.files.remove("report")
@@ -206,36 +113,6 @@ def test_cmake_project_do_checktestall(report_clang_build):
         assert f.contains('</mutations>')
     with READ(TEST.CHECKTEST("cmake-project", "index.html")) as f:
         assert f.contains(SENTINEL_HTML_TITLE)
-
-
-def test_qmake5_project_do_coverageall(report_clang_build):
-    report_clang_build.files.remove("report")
-
-    assert report_clang_build.shell.execute("bitbake qmake5-project -c coverageall").stderr.empty()
-
-    READ = report_clang_build.files.read
-
-    with READ(TEST.RESULT("qmake5-project", "test-qt5-gtest.xml")) as f:
-        assert f.contains('classname="qmake5-project.PlusTest"')
-        assert f.contains(NORMAL_GT_PLUS_TEST_FAILED_LOG)
-        assert f.contains('classname="qmake5-project.MinusTest"')
-        assert f.contains(NORMAL_GT_MINUS_TEST_FAILED_LOG)
-
-    with READ(TEST.RESULT("qmake5-project", "tests/plus_test/test_result.xml")) as f:
-        assert f.contains('name="qmake5-project.PlusTest"')
-        assert f.contains(QT_PLUS_TEST_FAILED_LOG)
-
-    with READ(TEST.RESULT("qmake5-project", "tests/minus_test/test_result.xml")) as f:
-        assert f.contains('name="qmake5-project.MinusTest"')
-        assert f.contains(QT_MINUS_TEST_FAILED_LOG)
-
-    assert READ(TEST.COVERAGE("qmake5-project", "index.html")).contains(LCOV_HTML_TITLE)
-
-    with READ(TEST.COVERAGE("qmake5-project", "coverage.xml")) as f:
-        assert f.contains('name="qmake5-project.plus.src"')
-        assert f.contains('<method name="arithmetic::plus(int, int)" signature="" line-rate="1.0" branch-rate="1.0">')
-        assert f.contains('name="qmake5-project.minus.src"')
-        assert f.contains('<method name="arithmetic::minus(int, int)" signature="" line-rate="1.0" branch-rate="1.0">')
 
 
 def test_qmake5_project_do_checkcodeall(report_clang_build):
@@ -258,28 +135,6 @@ def test_qmake5_project_do_checktestall(report_clang_build):
         assert f.contains(SENTINEL_HTML_TITLE)
 
 
-def test_autotools_project_do_coverageall(report_clang_build):
-    report_clang_build.files.remove("report")
-
-    assert report_clang_build.shell.execute("bitbake autotools-project -c coverageall").stderr.empty()
-
-    READ = report_clang_build.files.read
-
-    with READ(TEST.RESULT("autotools-project", "operatorTest.xml")) as f:
-        assert f.contains('classname="autotools-project.PlusTest"')
-        assert f.contains(NORMAL_GT_PLUS_TEST_FAILED_LOG)
-        assert f.contains('classname="autotools-project.MinusTest"')
-        assert f.contains(NORMAL_GT_MINUS_TEST_FAILED_LOG)
-
-    assert READ(TEST.COVERAGE("autotools-project", "index.html")).contains(LCOV_HTML_TITLE)
-
-    with READ(TEST.COVERAGE("autotools-project", "coverage.xml")) as f:
-        assert f.contains('name="autotools-project.plus.src"')
-        assert f.contains('<method name="arithmetic::plus(int, int)" signature="" line-rate="1.0" branch-rate="1.0">')
-        assert f.contains('name="autotools-project.minus.src"')
-        assert f.contains('<method name="arithmetic::minus(int, int)" signature="" line-rate="1.0" branch-rate="1.0">')
-
-
 def test_autotools_project_do_checkcodeall(report_clang_build):
     report_clang_build.files.remove("report")
     assert report_clang_build.shell.execute("bitbake autotools-project -c checkcodeall").stderr.empty()
@@ -298,92 +153,3 @@ def test_autotools_project_do_checktestall(report_clang_build):
     with READ(TEST.CHECKTEST("autotools-project", "index.html")) as f:
         assert f.contains(SENTINEL_HTML_TITLE)
 
-
-def test_humidifier_project_do_coverageall(report_clang_build):
-    report_clang_build.files.remove("report")
-
-    assert report_clang_build.shell.execute("bitbake humidifier-project -c coverageall").stderr.empty()
-
-    READ = report_clang_build.files.read
-
-    assert READ(TEST.RESULT("humidifier-project", "unittest.xml")).contains('classname="humidifier-project.HumidifierTest"')
-
-    assert READ(TEST.COVERAGE("humidifier-project", "index.html")).contains(LCOV_HTML_TITLE)
-
-    with READ(TEST.COVERAGE("humidifier-project", "coverage.xml")) as f:
-        assert f.contains('name="humidifier-project.humidifier.src"')
-        assert f.contains('<method name="Humidifier::setPreferredHumidity(int)" signature="" line-rate="1.0" branch-rate="1.0">')
-        assert f.contains('<method name="Atomizer_Set(int)" signature="" line-rate="1.0" branch-rate="1.0">')
-        assert f.contains('<method name="FakeHumiditySensor::getHumidityLevel() const" signature="" line-rate="1.0" branch-rate="1.0">')
-        assert f.contains('<method name="FakeHumiditySensor::gmock_getHumidityLevel() const" signature="" line-rate="1.0" branch-rate="1.0">')
-
-
-def test_humidifier_project_do_checkcodeall(report_clang_build):
-    report_clang_build.files.remove("report")
-    assert report_clang_build.shell.execute("bitbake humidifier-project -c checkcodeall").stderr.empty()
-    READ = report_clang_build.files.read
-    # assert READ(TEST.CHECK("humidifier-project", "cppcheck_report.xml")).contains(CPPCHECK_NO_ERRORS_FOUND)
-    assert READ(TEST.CHECK("humidifier-project", "cpplint_report.txt")).empty()
-    assert READ(TEST.CHECK("humidifier-project", "clang-tidy-report.txt")).empty()
-
-
-def test_humidifier_project_do_checktestall(report_clang_build):
-    report_clang_build.files.remove("report")
-    assert report_clang_build.shell.execute("bitbake humidifier-project -c checktestall").stderr.empty()
-    READ = report_clang_build.files.read
-    with READ(TEST.CHECKTEST("humidifier-project", "mutations.xml")) as f:
-        assert f.contains('</mutations>')
-    with READ(TEST.CHECKTEST("humidifier-project", "index.html")) as f:
-        assert f.contains(SENTINEL_HTML_TITLE)
-
-
-def test_sqlite3logger_do_coverageall(report_clang_build):
-    report_clang_build.files.remove("report")
-
-    assert report_clang_build.shell.execute("bitbake sqlite3logger -c coverageall").stderr.empty()
-
-    READ = report_clang_build.files.read
-
-    assert READ(TEST.RESULT("sqlite3wrapper", "SQLite3WrapperTest.exe.xml")).contains('classname="sqlite3wrapper.DatabaseTest"')
-    assert READ(TEST.COVERAGE("sqlite3wrapper", "index.html")).contains(LCOV_HTML_TITLE)
-    with READ(TEST.COVERAGE("sqlite3wrapper", "coverage.xml")) as f:
-        assert f.contains('name="sqlite3wrapper.src"')
-        assert f.contains('<method name="SQLite3Wrapper::Column::getName() const" signature="" line-rate="1.0" branch-rate="1.0">')
-        assert f.contains('<method name="SQLite3Wrapper::Statement::check(int)" signature="" line-rate="1.0" branch-rate="1.0">')
-        assert f.contains('<method name="SQLite3Wrapper::Database::check(int)" signature="" line-rate="1.0" branch-rate="1.0">')
-
-    assert READ(TEST.RESULT("stringutils", "unittest.bin.xml")).contains('classname="stringutils.StringTest"')
-    assert READ(TEST.COVERAGE("stringutils", "index.html")).contains(LCOV_HTML_TITLE)
-    with READ(TEST.COVERAGE("stringutils", "coverage.xml")) as f:
-        assert f.contains('name="stringutils.include.util"')
-        assert f.contains('<method name="bool util::string::contains&lt;char&gt;(char const*, char const*)" signature="" line-rate="1.0" branch-rate="1.0">')
-
-
-def test_sqlite3logger_do_checkcodeall(report_clang_build):
-    report_clang_build.files.remove("report")
-
-    assert report_clang_build.shell.execute("bitbake sqlite3logger -c checkcodeall").stderr.empty()
-
-    READ = report_clang_build.files.read
-
-    # assert READ(TEST.CHECK("sqlite3wrapper", "cppcheck_report.xml")).contains(CPPCHECK_NO_ERRORS_FOUND)
-    assert READ(TEST.CHECK("sqlite3wrapper", "cpplint_report.txt")).empty()
-    assert READ(TEST.CHECK("sqlite3wrapper", "clang-tidy-report.txt")).empty()
-
-    # assert READ(TEST.CHECK("stringutils", "cppcheck_report.xml")).contains(CPPCHECK_NO_ERRORS_FOUND)
-    assert READ(TEST.CHECK("stringutils", "cpplint_report.txt")).empty()
-    assert READ(TEST.CHECK("stringutils", "clang-tidy-report.txt")).empty()
-
-
-def test_sqlite3logger_project_do_checktestall(report_clang_build):
-    report_clang_build.files.remove("report")
-    assert report_clang_build.shell.execute("bitbake sqlite3logger -c checktestall").stderr.empty()
-    READ = report_clang_build.files.read
-    with READ(TEST.CHECKTEST("sqlite3wrapper", "mutations.xml")) as f:
-        assert f.contains('</mutations>')
-    with READ(TEST.CHECKTEST("sqlite3wrapper", "index.html")) as f:
-        assert f.contains(SENTINEL_HTML_TITLE)
-    with READ(TEST.CHECKTEST("stringutils", "mutations.xml")) as f:
-        assert f.contains('</mutations>')
-    with READ(TEST.CHECKTEST("stringutils", "index.html")) as f:
-        assert f.contains(SENTINEL_HTML_TITLE)
