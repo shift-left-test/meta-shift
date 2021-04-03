b37427f67f8f7adee32cecac98f9ea27e48c6700
sangmo.kang <sangmo.kang@lge.com>
2021-04-03 12:31:33 +0900

    #422 fix not to check src_uri_checksum rule When downloading src through the configuration management system

--- lib/recipetool/shift_oelint_adv/rule_base/rule_var_src_uri_checksum.py
+++ lib/recipetool/shift_oelint_adv/rule_base/rule_var_src_uri_checksum.py
@@ -34,7 +34,7 @@ class VarSRCUriOptions(Rule):
                     if x == INLINE_BLOCK:
                         continue
                     _url = get_scr_components(x)
-                    if _url["scheme"] in ["http", "https", "ftp", "ftps", "sftp", "s3", "git", "repo", "svn"]:
+                    if _url["scheme"] in ["http", "https", "ftp", "ftps", "sftp", "s3"]:
                         name = ""
                         if "name" in _url["options"]:
                             name = _url["options"]["name"]
@@ -71,3 +71,4 @@ class VarSRCUriOptions(Rule):
                 res += self.finding(filename, filelines, message)
 
         return res
+
