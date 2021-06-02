c9c81be90c34c82612127307f235393e0ad5dadf
sangmo.kang <sangmo.kang@lge.com>
2021-06-03 00:14:12 +0900

    #477 remove json option of bitbake-layers-inspect

--- test/bitbakelayers_inspect_test.py
+++ test/bitbakelayers_inspect_test.py
@@ -52,45 +52,19 @@ def test_default_format(bare_build):
                                 "Classes:")
 
 
-def test_default_format_save_as_file(bare_build):
-    d = tempfile.mkdtemp()
-    try:
-        temp = os.path.join(d, "output.plain")
-        o = bare_build.shell.execute("bitbake-layers inspect meta-poky --output {}".format(temp))
-        assert not o.stdout.contains("Name: yocto")
-        with open(temp, "r") as f:
-            assert "Name: yocto" in f.read()
-    finally:
-        shutil.rmtree(d)
-
-
-def test_json_format(bare_build):
-    o = bare_build.shell.execute("bitbake-layers inspect meta-poky --json")
-    assert o.stdout.containsAll('"General Information": {{',
-                                '"Layer": "meta-poky"',
-                                '"Name": "yocto"',
-                                '"Path":',
-                                '"Conf":',
-                                '"Priority": "5"',
-                                '"Version": "3"',
-                                '"Compatibilities":',
-                                '"Dependencies": "core"',
-                                '"Additional Information": {{',
-                                '"Images": []',
-                                '"Machines": []',
-                                '"Distros": [',
-                                '"Classes": [')
-
-
 def test_json_format_save_as_file(bare_build):
     d = tempfile.mkdtemp()
     try:
         temp = os.path.join(d, "output.json")
-        o = bare_build.shell.execute("bitbake-layers inspect meta-poky --json --output {}".format(temp))
+        o = bare_build.shell.execute("bitbake-layers inspect meta-poky --output {}".format(temp))
         assert not o.stdout.contains('"Name": "yocto"')
         with open(temp, "r") as f:
             data = json.load(f)
             assert data["General Information"]["Name"] == "yocto"
+            assert data["General Information"]["Layer"] == "meta-poky"
+            assert data["General Information"]["Priority"] == "5"
+            assert data["General Information"]["Version"] == "3"
+            assert data["General Information"]["Dependencies"] == "core"
     finally:
         shutil.rmtree(d)
 
