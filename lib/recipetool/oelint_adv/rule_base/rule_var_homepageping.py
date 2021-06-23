a97cdf0ca026b171207d2f2a38e8b42a57290302
sangmo.kang <sangmo.kang@lge.com>
2021-06-23 18:26:25 +0900

    #490 recipetool-check: Updates to oelint-adv version 2.52

---
+++ lib/recipetool/oelint_adv/rule_base/rule_var_homepageping.py
@@ -0,0 +1,36 @@
+from oelint_parser.cls_item import Variable
+from oelint_adv.cls_rule import Rule
+
+try:
+    from urllib.request import urlopen, Request
+    from urllib.error import HTTPError, URLError
+except ImportError:
+    from urllib2 import urlopen, Request
+    from urllib2 import HTTPError, URLError
+
+
+class VarHomepagePing(Rule):
+    def __init__(self):
+        super(VarHomepagePing, self).__init__(id="oelint.vars.homepageping",
+                         severity="warning",
+                         message="'HOMEPAGE' isn't reachable")
+
+    def check(self, _file, stash):
+        res = []
+        items = stash.GetItemsFor(filename=_file, classifier=Variable.CLASSIFIER,
+                                  attribute=Variable.ATTR_VAR, attributeValue="HOMEPAGE")
+        for i in items:
+            try:
+                req = Request(i.VarValueStripped)
+                try:
+                    urlopen(req, timeout=4)
+                except HTTPError as e:
+                    if e.code == 404: # pragma: no cover
+                        res += self.finding(i.Origin, i.InFileLine) # pragma: no cover
+                except URLError:
+                    res += self.finding(i.Origin, i.InFileLine)
+            except ValueError:
+                res += self.finding(i.Origin, i.InFileLine)
+            except Exception: # pragma: no cover
+                pass # pragma: no cover
+        return res
