1e9ea7fc3f49639c7f508cabf872d0928f680f70
sangmo.kang <sangmo.kang@lge.com>
2021-06-23 18:26:33 +0900

    #490 recipetool-check: Updates to oelint-adv version 2.52

---
+++ lib/recipetool/oelint_adv/rule_base/rule_var_license_remote.py
@@ -0,0 +1,25 @@
+from oelint_parser.cls_item import Variable
+from oelint_adv.cls_rule import Rule
+from oelint_parser.helper_files import get_scr_components, expand_term
+
+
+class VarLicenseRemoteFile(Rule):
+    def __init__(self):
+        super(VarLicenseRemoteFile, self).__init__(id="oelint.var.licenseremotefile",
+                         severity="warning",
+                         message="License-File should be a remote file")
+
+    def check(self, _file, stash):
+        res = []
+        _items = stash.GetItemsFor(filename=_file, classifier=Variable.CLASSIFIER,
+                                   attribute=Variable.ATTR_VAR, attributeValue="LIC_FILES_CHKSUM")
+        _var_whitelist = ["${WORKDIR}", "${S}", "${B}"]
+        for i in _items:
+            components = get_scr_components(expand_term(stash, _file, i.VarValueStripped))
+            if any(components) and components["scheme"] == "file":
+                _clean = components["src"]
+                for x in _var_whitelist:
+                    _clean = _clean.replace(x, "")
+                if "${" in _clean:
+                    res += self.finding(i.Origin, i.InFileLine)
+        return res
