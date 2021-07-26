7cce33c54e807b6e28d2336c2d2feca67be19f43
sangmo.kang <sangmo.kang@lge.com>
2021-07-26 18:42:05 +0900

    #502 remove CWE related code

--- test/externalsrc_test.py
+++ test/externalsrc_test.py
@@ -59,7 +59,6 @@ def test_cmake_project_do_checkcode(test_build):
         assert o.stdout.contains("cmake-project-1.0.0-r0 do_checkcode: INFO:SAGE:* duplo is running...")
         assert o.stdout.contains("cmake-project-1.0.0-r0 do_checkcode: INFO:SAGE:* cppcheck is running...")
         assert o.stdout.contains("cmake-project-1.0.0-r0 do_checkcode: INFO:SAGE:* cpplint is running...")
-        assert o.stdout.contains("cmake-project-1.0.0-r0 do_checkcode: INFO:SAGE:* flawfinder is running...")
 
 
 def test_cmake_project_do_checkcache(test_build):
@@ -85,3 +84,4 @@ def test_oelint_adv_native_project_do_build(test_build):
     with externalsrc_execute(test_build, "oelint-adv-native", "build") as o:
         assert o.stderr.empty()
         assert o.returncode == 0
+
