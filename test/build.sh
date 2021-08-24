a930cab7fef8979ef4ca88e2f20c0322bd6c5326
Sung Gon Kim <sunggon82.kim@lge.com>
2021-08-24 13:54:19 +0900

    #511 Adds the build.sh to test builds

---
+++ test/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [[ -z ${1} ]]; then
+    echo "Missing the configuration argument."
+    echo "./build.sh <config> <build>"
+    exit 1
+fi
+if [[ -z ${2} ]]; then
+    echo "Missing the build directory argument."
+    echo "./build.sh <config> <build>"
+    exit 1
+fi
+
+./test/mini-mcf.py -c test/conf/${1} -d ${2} || exit 1
+source /tmp/meta-shift-repos-sunggon82.kim/poky/oe-init-build-env ${2}
+export BB_ENV_EXTRAWHITE="$BB_ENV_EXTRAWHITE DL_DIR SSTATE_DIR"
+bitbake core-image-minimal --runall=fetch
+bitbake core-image-minimal -c reportall --runall=fetch
+bitbake core-image-minimal -c populate_sdk --runall=fetch
+bitbake core-image-minimal
+bitbake core-image-minimal -c reportall
+bitbake core-image-minimal -c populate_sdk
