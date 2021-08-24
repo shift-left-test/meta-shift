387ab6c15ab2b16e2001214a44ad025d38e2fb26
Sung Gon Kim <sunggon82.kim@lge.com>
2021-08-24 14:22:54 +0900

    #511 Fixes the build.sh failure issue

--- test/build.sh
+++ test/build.sh
@@ -11,9 +11,9 @@ if [[ -z ${2} ]]; then
 fi
 
 ./test/mini-mcf.py -c test/conf/${1} -d ${2} || exit 1
-source /tmp/meta-shift-repos-sunggon82.kim/poky/oe-init-build-env ${2}
+source /tmp/meta-shift-repos-${USER}/poky/oe-init-build-env ${2}
 export BB_ENV_EXTRAWHITE="$BB_ENV_EXTRAWHITE DL_DIR SSTATE_DIR"
-bitbake core-image-minimal -c fetchall
-bitbake core-image-minimal
-bitbake core-image-minimal -c reportall
-bitbake core-image-minimal -c populate_sdk
+bitbake core-image-minimal -c fetchall || exit 1
+bitbake core-image-minimal || exit 1
+bitbake core-image-minimal -c reportall || exit 1
+bitbake core-image-minimal -c populate_sdk || exit 1
