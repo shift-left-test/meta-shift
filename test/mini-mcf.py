50c777e45d77ac939025c4d1cafa33d11cc65ef3
sangmo.kang <sangmo.kang@lge.com>
2021-08-26 10:59:08 +0900

    #536 restore distro to poky

--- test/mini-mcf.py
+++ test/mini-mcf.py
@@ -138,7 +138,7 @@ BBLAYERS ?= " \\
 def configure_local(conf_dir, conf_data):
     logger.info("Creating 'local.conf'...")
     LOCAL_CONF = '''MACHINE ??= "qemuarm64"
-DISTRO = "no-uninative"
+DISTRO = "poky"
 PACKAGE_CLASSES = "package_ipk"
 EXTRA_IMAGE_FEATURES = ""
 USER_CLASSES = ""
