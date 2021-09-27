4833a990ba4cbf10e4c784fb0879281ec77f02b0
Sung Gon Kim <sunggon82.kim@lge.com>
2021-09-27 17:19:33 +0900

    #568 selftest: Adds matches() and matchesAll() methods

--- test/bitbakelayers_test_recipes_test.py
+++ test/bitbakelayers_test_recipes_test.py
@@ -30,12 +30,12 @@ import pytest
 
 def test_show_recipes(test_build):
     o = test_build.shell.execute("recipetool test-recipes")
-    assert o.stdout.containsAll("autotools-project              1.0.0                meta-sample",
-                                "cmake-project                  1.0.0                meta-sample",
-                                "humidifier-project             1.0.0                meta-sample",
-                                "qmake-project                  1.0.0                meta-sample",
-                                "sqlite3wrapper                 0.1.0                meta-sample",
-                                "stringutils                    0.0.1                meta-sample")
+    assert o.stdout.matchesAll("autotools-project[ ]+1.0.0[ ]+meta-sample",
+                               "cmake-project[ ]+1.0.0[ ]+meta-sample",
+                               "humidifier-project[ ]+1.0.0[ ]+meta-sample",
+                               "qmake-project[ ]+1.0.0[ ]+meta-sample",
+                               "sqlite3wrapper[ ]+0.1.0[ ]+meta-sample",
+                               "stringutils[ ]+0.0.1[ ]+meta-sample")
 
 
 def test_show_no_recipes_without_test_enabled(release_build):
@@ -50,12 +50,12 @@ def test_show_no_recipes_without_test_enabled(release_build):
 
 def test_show_recipes_with_pnspec(test_build):
     o = test_build.shell.execute("recipetool test-recipes *-project")
-    assert o.stdout.containsAll("autotools-project              1.0.0                meta-sample",
-                                "cmake-project                  1.0.0                meta-sample",
-                                "humidifier-project             1.0.0                meta-sample",
-                                "qmake-project                  1.0.0                meta-sample")
-    assert not o.stdout.containsAll("sqlite3wrapper                 0.1.0                meta-sample",
-                                    "stringutils                    0.0.1                meta-sample")
+    assert o.stdout.matchesAll("autotools-project[ ]+1.0.0[ ]+meta-sample",
+                               "cmake-project[ ]+1.0.0[ ]+meta-sample",
+                               "humidifier-project[ ]+1.0.0[ ]+meta-sample",
+                               "qmake-project[ ]+1.0.0[ ]+meta-sample")
+    assert not o.stdout.matchesAll("sqlite3wrapper[ ]+0.1.0[ ]+meta-sample",
+                                   "stringutils[ ]+0.0.1[ ]+meta-sample")
 
 
 def test_show_no_recipes_without_test_enabled(release_build):
