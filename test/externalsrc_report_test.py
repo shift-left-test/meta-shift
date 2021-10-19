d4af2ba39a20572cd247b9e1d65103b0feaf2932
sangmo.kang <sangmo.kang@lge.com>
2021-10-19 04:08:41 +0000

    #584 fix not to fail *_checkrecipe

--- test/externalsrc_report_test.py
+++ test/externalsrc_report_test.py
@@ -157,7 +157,6 @@ def test_cmake_project_do_checkrecipe(report_build):
     READ = report_build.files.read
 
     with READ(REPORT.CHECKRECIPE("cmake-project", "recipe_violations.json")) as f:
-        assert f.contains('cmake-project_1.0.0.bb')
         assert f.contains('cmake-project_1.0.0.bbappend')
 
     assert READ(REPORT.ROOT("cmake-project", "metadata.json")).contains(METADATA_S)
