a7aa2f33c7be6c22f5c951d6b35fc2fbac20e043
Sung Gon Kim <sunggon82.kim@lge.com>
2021-10-28 01:25:08 +0000

    #571 Updates license information

--- test/externalsrc_report_test.py
+++ test/externalsrc_report_test.py
@@ -4,7 +4,7 @@
 """
 MIT License
 
-Copyright (c) 2020 Sangmo Kang
+Copyright (c) 2020 LG Electronics, Inc.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
