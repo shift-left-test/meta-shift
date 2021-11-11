ca483183d6a4b790696e63cbd5526b2595cd6d9d
Sung Gon Kim <sunggon82.kim@lge.com>
2021-11-12 08:48:26 +0900

    #590 Uses the OWN_MIRROR_URL environment variable to set up the cache server

--- test/mini-mcf.py
+++ test/mini-mcf.py
@@ -169,6 +169,14 @@ INHIBIT_PACKAGE_DEBUG_SPLIT = "1"
         with open(local_conf, "a") as f:
             f.write('{} ?= "{}"\n'.format(key, value))
 
+    if "OWN_MIRROR_URL" in os.environ:
+        url = os.environ.get("OWN_MIRROR_URL")
+        with open(local_conf, "a") as f:
+            f.write('INHERIT_append ?= " own-mirrors uninative"\n')
+            f.write('SOURCE_MIRROR_URL ?= "{}/{}/downloads"\n'.format(url, BRANCH))
+            f.write('SSTATE_MIRRORS ?= "file://.* {}/{}/sstate-cache/PATH"\n'.format(url, BRANCH))
+            f.write('UNINATIVE_URL ?= "{}/{}/downloads/uninative/${{@d.getVarFlag(\'UNINATIVE_CHECKSUM\', d.getVar(\'BUILD_ARCH\', True), True)}}/"'.format(url, BRANCH))
+
 
 def configure(args):
     def read_json(filename):
