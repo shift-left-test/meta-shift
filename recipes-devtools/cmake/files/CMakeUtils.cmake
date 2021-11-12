eb6dfafb5465bca56ecede075f0206400047bdae
sangmo.kang <sangmo.kang@lge.com>
2021-11-12 05:31:35 +0000

    #610 Updates LICENSE information of CMakeUtils

--- recipes-devtools/cmake/files/CMakeUtils.cmake
+++ recipes-devtools/cmake/files/CMakeUtils.cmake
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (c) 2019 Sung Gon Kim
+# Copyright (c) 2019 LG Electronics, Inc.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
