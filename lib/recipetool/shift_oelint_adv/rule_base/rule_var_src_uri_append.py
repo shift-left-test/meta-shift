f3ab309cbae1483b83334a9f770380ba1947d8da
sangmo.kang <sangmo.kang@lge.com>
2021-11-30 15:34:51 +0900

    #616 Updates oe-lint-adv and oe-lint-parser

---
+++ lib/recipetool/shift_oelint_adv/rule_base/rule_var_src_uri_append.py
@@ -0,0 +1,26 @@
+from shift_oelint_parser.cls_item import Variable
+from shift_oelint_adv.cls_rule import Rule
+
+
+class VarSRCUriGitTag(Rule):
+    def __init__(self):
+        super(VarSRCUriGitTag, self).__init__(id='oelint.vars.srcuriappend',
+                         severity='error',
+                         message='Use SRC_URI_append otherwise this will override weak defaults by inherit')
+
+    def check(self, _file, stash):
+        res = []
+        inherits = stash.GetItemsFor(filename=_file, classifier=Variable.CLASSIFIER,
+                                     attribute=Variable.ATTR_VAR, attributeValue='inherit')
+        if not inherits:
+            return res
+
+        items = stash.GetItemsFor(filename=_file, classifier=Variable.CLASSIFIER,
+                                  attribute=Variable.ATTR_VAR, attributeValue='SRC_URI')
+        for item in items:
+            if any([item.Flag.endswith(x) for x in ['md5sum', 'sha256sum']]):
+                # These are just the hashes
+                continue
+            if item.VarOp in [' += ']:
+                res += self.finding(item.Origin, item.InFileLine)
+        return res
