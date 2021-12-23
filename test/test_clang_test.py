13bb7266fa799fef615cd3d1e9ab8599a792e310
Sung Gon Kim <sunggon82.kim@lge.com>
2021-12-23 15:50:54 +0900

    #620 Removes unused recipes

--- test/test_clang_test.py
+++ test/test_clang_test.py
@@ -40,11 +40,11 @@ def test_core_image_minimal_do_checktestall(test_clang_build):
 
 def test_do_checktest_seed_option(test_clang_build):
     o = test_clang_build.shell.execute("bitbake cmake-project -c checktest")
-    assert o.stdout.contains("cmake-project/1.0.0-r0/git/plus/src/plus.cpp,plus,30,12,30,13,*")
+    assert o.stdout.contains("cmake-project/1.0.0-r0/git/minus/src/minus.cpp,minus,30,12,30,13,*")
     assert o.stdout.contains("cmake-project/1.0.0-r0/git/program/main.cpp,main,37,39,37,40,*")
 
     o = test_clang_build.shell.execute("bitbake cmake-project -c checktestall")
-    assert o.stdout.contains("cmake-project/1.0.0-r0/git/plus/src/plus.cpp,plus,30,12,30,13,*")
+    assert o.stdout.contains("cmake-project/1.0.0-r0/git/minus/src/minus.cpp,minus,30,12,30,13,*")
     assert o.stdout.contains("cmake-project/1.0.0-r0/git/program/main.cpp,main,37,39,37,40,*")
 
 
