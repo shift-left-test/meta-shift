e5efedfa2ba913415a130d24fe30355d9fc41bed
Sung Gon Kim <sunggon82.kim@lge.com>
2022-06-29 23:31:54 +0900

    #673 selftest: Reduce the number of fixtures for faster test execution

--- test/devtool_cache_test.py
+++ test/devtool_cache_test.py
@@ -11,42 +11,42 @@ import shutil
 import tempfile
 
 
-def test_unknown_recipe(bare_build):
-    o = bare_build.shell.execute("devtool cache unknown-recipe")
+def test_unknown_recipe(release_build):
+    o = release_build.shell.execute("devtool cache unknown-recipe")
     assert o.stdout.contains("ERROR: Nothing PROVIDES 'unknown-recipe'")
 
 
-def test_cache(bare_build):
-    o = bare_build.shell.execute("devtool cache cmake-native")
+def test_cache(release_build):
+    o = release_build.shell.execute("devtool cache cmake-native")
     assert o.stdout.matches(r"Wanted : [0-9]+ \([0-9]+%\)")
     assert o.stdout.matches(r"Found  : [0-9]+ \([0-9]+%\)")
     assert o.stdout.matches(r"Missed : [0-9]+ \([0-9]+%\)")
 
 
-def test_cache_with_details(bare_build):
-    o = bare_build.shell.execute("devtool cache cmake-native --found --missed")
+def test_cache_with_details(release_build):
+    o = release_build.shell.execute("devtool cache cmake-native --found --missed")
     assert o.stdout.containsAll("cmake-native:do_populate_lic",
                                 "cmake-native:do_populate_sysroot",
                                 "cmake-native")
 
 
-def test_cache_with_unknown_cmd_option(bare_build):
-    o = bare_build.shell.execute("devtool cache cmake-native -c unknown_task")
+def test_cache_with_unknown_cmd_option(release_build):
+    o = release_build.shell.execute("devtool cache cmake-native -c unknown_task")
     assert o.stdout.contains("ERROR: Task do_unknown_task does not exist")
 
 
-def test_cache_with_known_cmd_option(bare_build):
-    o = bare_build.shell.execute("devtool cache cmake-native -c configure")
+def test_cache_with_known_cmd_option(release_build):
+    o = release_build.shell.execute("devtool cache cmake-native -c configure")
     assert o.stdout.matches(r"Wanted : [0-9]+ \([0-9]+%\)")
     assert o.stdout.matches(r"Found  : [0-9]+ \([0-9]+%\)")
     assert o.stdout.matches(r"Missed : [0-9]+ \([0-9]+%\)")
 
 
-def test_cache_save_as_file(bare_build):
+def test_cache_save_as_file(release_build):
     d = tempfile.mkdtemp()
     try:
         report_file_path = os.path.join(d, "cmake-native_cache_check.json")
-        bare_build.shell.execute("devtool cache cmake-native -o=%s" % report_file_path)
+        release_build.shell.execute("devtool cache cmake-native -o=%s" % report_file_path)
 
         with open(report_file_path) as f:
             data = json.load(f)
@@ -60,22 +60,22 @@ def test_cache_save_as_file(bare_build):
         shutil.rmtree(d)
 
 
-def test_cache_save_as_file_with_unknown_cmd_option(bare_build):
+def test_cache_save_as_file_with_unknown_cmd_option(release_build):
     d = tempfile.mkdtemp()
     try:
         report_file_path = os.path.join(d, "cmake-native_cache_check.json")
-        o = bare_build.shell.execute("devtool cache cmake-native -c unknown_task -o=%s" % report_file_path)
+        o = release_build.shell.execute("devtool cache cmake-native -c unknown_task -o=%s" % report_file_path)
         assert o.stdout.contains("ERROR: Task do_unknown_task does not exist")
         assert not os.path.exists(report_file_path)
     finally:
         shutil.rmtree(d)
 
 
-def test_cache_save_as_file_with_known_cmd_option(bare_build):
+def test_cache_save_as_file_with_known_cmd_option(release_build):
     d = tempfile.mkdtemp()
     try:
         report_file_path = os.path.join(d, "cmake-native_configure_cache_check.json")
-        bare_build.shell.execute("devtool cache cmake-native -c configure -o=%s" % report_file_path)
+        release_build.shell.execute("devtool cache cmake-native -c configure -o=%s" % report_file_path)
 
         with open(report_file_path) as f:
             data = json.load(f)
