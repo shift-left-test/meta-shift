1741285a6731c86af673601d62181a13e2b92771
Sung Gon Kim <sunggon82.kim@lge.com>
2022-06-30 09:35:40 +0900

    #673 selftest: Refactor test scripts

--- test/recipetool_check_test.py
+++ test/recipetool_check_test.py
@@ -5,10 +5,6 @@ Copyright (c) 2020 LG Electronics Inc.
 SPDX-License-Identifier: MIT
 """
 
-import os
-import json
-import shutil
-import tempfile
 import pytest
 
 
@@ -20,20 +16,15 @@ def test_check(release_build):
 
 
 def test_check_save_as_file(release_build):
-    d = tempfile.mkdtemp()
-    try:
-        temp = os.path.join(d, "output.json")
-        o = release_build.shell.execute("recipetool check cmake-native --output {}".format(temp))
-        with open(temp, "r") as f:
-            data = json.load(f)
-            assert isinstance(data["issues"], list)
-            assert len(data["issues"]) > 0
-            for issue in data["issues"]:
-                assert isinstance(issue, dict)
-                assert isinstance(issue["file"], str)
-                assert isinstance(issue["line"], int)
-                assert isinstance(issue["severity"], str)
-                assert isinstance(issue["rule"], str)
-                assert isinstance(issue["description"], str)
-    finally:
-        shutil.rmtree(d)
+    with release_build.files.tempfile("report.json") as f:
+        o = release_build.shell.execute("recipetool check cmake-native --output {}".format(f))
+        data = release_build.files.asJson("report.json")
+        assert isinstance(data["issues"], list)
+        assert len(data["issues"]) > 0
+        for issue in data["issues"]:
+            assert isinstance(issue, dict)
+            assert isinstance(issue["file"], str)
+            assert isinstance(issue["line"], int)
+            assert isinstance(issue["severity"], str)
+            assert isinstance(issue["rule"], str)
+            assert isinstance(issue["description"], str)
