17ccffb4dbad7ea5ad89729e9d89fa6314161770
sangmo.kang <sangmo.kang@lge.com>
2022-07-07 20:45:55 +0900

    #677 Supports for Unix shell-style wildcards in SHIFT_COVERAGE_EXCLUDES

---
+++ test/option_coverage_excludes_test.py
@@ -0,0 +1,47 @@
+#-*- coding: utf-8 -*-
+
+"""
+Copyright (c) 2020 LG Electronics Inc.
+SPDX-License-Identifier: MIT
+"""
+
+import pytest
+
+
+def test_cmake_project_coverage_filter(report_build):
+    with report_build.files.conf() as conf:
+        conf.set("SHIFT_COVERAGE_EXCLUDES", "program */src/*.cpp test/MinusTest.cpp")
+        assert report_build.shell.execute("bitbake cmake-project -c coverage").stderr.empty()
+        
+        coverage = report_build.files.asXml("report/cmake-project-1.0.0-r0/coverage/coverage.xml")
+        assert coverage.containsElementWithAttrib("class", {"filename":"test/PlusTest.cpp"})
+        assert not coverage.containsElementWithAttrib("class", {"filename":"program/main.cpp"})
+        assert not coverage.containsElementWithAttrib("class", {"filename":"plus/src/plus.cpp"})
+        assert not coverage.containsElementWithAttrib("class", {"filename":"minius/src/minus.cpp"})
+        assert not coverage.containsElementWithAttrib("class", {"filename":"test/MinusTest.cpp"})
+
+
+def test_qmake_project_coverage_filter(report_build):
+    with report_build.files.conf() as conf:
+        conf.set("SHIFT_COVERAGE_EXCLUDES", "program */src/*.cpp test/MinusTest.cpp")
+        assert report_build.shell.execute("bitbake qmake-project -c coverage").stderr.empty()
+        
+        coverage = report_build.files.asXml("report/qmake-project-1.0.0-r0/coverage/coverage.xml")
+        assert coverage.containsElementWithAttrib("class", {"filename":"test/PlusTest.cpp"})
+        assert not coverage.containsElementWithAttrib("class", {"filename":"program/main.cpp"})
+        assert not coverage.containsElementWithAttrib("class", {"filename":"plus/src/plus.cpp"})
+        assert not coverage.containsElementWithAttrib("class", {"filename":"minius/src/minus.cpp"})
+        assert not coverage.containsElementWithAttrib("class", {"filename":"test/MinusTest.cpp"})
+
+
+def test_autotools_project_coverage_filter(report_build):
+    with report_build.files.conf() as conf:
+        conf.set("SHIFT_COVERAGE_EXCLUDES", "program */src/*.cpp test/MinusTest.cpp")
+        assert report_build.shell.execute("bitbake autotools-project -c coverage").stderr.empty()
+        
+        coverage = report_build.files.asXml("report/autotools-project-1.0.0-r0/coverage/coverage.xml")
+        assert coverage.containsElementWithAttrib("class", {"filename":"test/PlusTest.cpp"})
+        assert not coverage.containsElementWithAttrib("class", {"filename":"program/main.cpp"})
+        assert not coverage.containsElementWithAttrib("class", {"filename":"plus/src/plus.cpp"})
+        assert not coverage.containsElementWithAttrib("class", {"filename":"minius/src/minus.cpp"})
+        assert not coverage.containsElementWithAttrib("class", {"filename":"test/MinusTest.cpp"})
