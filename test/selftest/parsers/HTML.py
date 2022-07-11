9359b86a3e1b8df68407de1711e9d819e6d3a71b
Sung Gon Kim <sunggon82.kim@lge.com>
2022-07-11 22:55:41 +0900

    #673 selftest: Refactor HTML parser

--- test/selftest/parsers/HTML.py
+++ test/selftest/parsers/HTML.py
@@ -5,21 +5,40 @@ Copyright (c) 2022 LG Electronics Inc.
 SPDX-License-Identifier: MIT
 """
 
-from selftest.parsers import XML
-import re
+from html.parser import HTMLParser
+from selftest.parsers.data import multidict
+
+
+class HTMLReportParser(HTMLParser):
+    def __init__(self, data):
+        super(HTMLReportParser, self).__init__()
+        self.data = data
+        self.tags = []
+
+    def add(self, value):
+        self.data["/".join(self.tags)] = value
+
+    def handle_starttag(self, tag, attrs):
+        # Ignore HTML void elements
+        # https://www.w3.org/TR/2011/WD-html-markup-20110113/syntax.html#syntax-elements
+        if tag in ["area", "base", "br", "col", "command", "embed", "hr", "img", "input", "keygen", "link", "meta", "param", "source", "track", "wbr"]:
+            return
+        self.tags.append(tag)
+        if attrs:
+            self.add(dict(attrs))
+
+    def handle_endtag(self, tag):
+        if self.tags[-1] == tag:
+            self.tags.pop()
+
+    def handle_data(self, data):
+        if data.strip():
+            self.add(data.strip())
 
 
 def parse(path):
     with open(path, "r") as f:
-        xmltext = f.read()
-
-        # To avoid failing when parsing html
-        xmltext = xmltext.replace("&nbsp;%"," ")
-        xmltext = re.sub("<![^>\n]*>", "", xmltext)
-        xmltext = re.sub("=([0-9]+)", r'="\1"',xmltext)
-        xmltext = re.sub("<meta[^>\n]*>", "", xmltext)
-        xmltext = re.sub("<link[^>\n]*>", "", xmltext)
-        xmltext = re.sub("<img[^>\n]*>", "", xmltext)
-        xmltext = re.sub("<br>", "", xmltext)
-
-        return XML.parseString(xmltext)
+        data = multidict()
+        parser = HTMLReportParser(data)
+        parser.feed(f.read())
+        return data
