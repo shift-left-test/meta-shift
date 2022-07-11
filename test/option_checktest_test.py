4a833826484151a12d3efe9c8b5e276def084c2d
Sung Gon Kim <sunggon82.kim@lge.com>
2022-07-11 14:35:18 +0900

    #685 Change test cases

--- test/option_checktest_test.py
+++ test/option_checktest_test.py
@@ -25,8 +25,8 @@ def test_do_checktest_seed_option(report_build):
 
 def test_do_checktest_verbose_option(report_build):
     with report_build.files.conf() as conf:
+        conf.set("SHIFT_CHECKTEST_SEED", "1234")
         conf.set("SHIFT_CHECKTEST_VERBOSE", "1")
-        o = report_build.shell.execute("bitbake autotools-project -c checktest")
-        assert o.stdout.contains("autotools-project-1.0.0-r0 do_checktest: CommandMutate [INFO] mutant:")
-        assert o.stdout.contains("autotools-project-1.0.0-r0 do_checktest: CommandEvaluate [INFO] mutant:")
-        assert o.stdout.contains("autotools-project-1.0.0-r0 do_checktest: CommandReport [INFO] evaluation-file")
+
+        o = report_build.shell.execute("bitbake cmake-project -c checktest")
+        assert o.stdout.contains("cmake-project-1.0.0-r0 do_checktest: CommandPopulate [INFO] random seed:1234")
