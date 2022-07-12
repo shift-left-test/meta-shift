2f2b3c08c562c1e60d439432f852f7bb2fbf9d0d
Sung Gon Kim <sunggon82.kim@lge.com>
2022-07-12 13:43:46 +0900

    #676 selftest: Refactor XML parser

--- test/option_coverage_test.py
+++ test/option_coverage_test.py
@@ -12,36 +12,36 @@ def test_cmake_project_coverage_filter(report_build):
     with report_build.files.conf() as conf:
         conf.set("SHIFT_COVERAGE_EXCLUDES", "program */src/*.cpp test/MinusTest.cpp")
         assert report_build.shell.execute("bitbake cmake-project -c coverage").stderr.empty()
-        
+
         coverage = report_build.files.readAsXml("report/cmake-project-1.0.0-r0/coverage/coverage.xml")
-        assert coverage.containsElementWithAttrib("class", {"filename":"test/PlusTest.cpp"})
-        assert not coverage.containsElementWithAttrib("class", {"filename":"program/main.cpp"})
-        assert not coverage.containsElementWithAttrib("class", {"filename":"plus/src/plus.cpp"})
-        assert not coverage.containsElementWithAttrib("class", {"filename":"minius/src/minus.cpp"})
-        assert not coverage.containsElementWithAttrib("class", {"filename":"test/MinusTest.cpp"})
+        assert coverage["coverage/packages/package/classes/class"]["filename"] != "minus/src/minus.cpp"
+        assert coverage["coverage/packages/package/classes/class"]["filename"] != "plus/src/plus.cpp"
+        assert coverage["coverage/packages/package/classes/class"]["filename"] != "program/main.cpp"
+        assert coverage["coverage/packages/package/classes/class"]["filename"] != "test/MinusTest.cpp"
+        assert coverage["coverage/packages/package/classes/class"]["filename"] == "test/PlusTest.cpp"
 
 
 def test_qmake_project_coverage_filter(report_build):
     with report_build.files.conf() as conf:
         conf.set("SHIFT_COVERAGE_EXCLUDES", "program */src/*.cpp test/MinusTest.cpp")
         assert report_build.shell.execute("bitbake qmake-project -c coverage").stderr.empty()
-        
+
         coverage = report_build.files.readAsXml("report/qmake-project-1.0.0-r0/coverage/coverage.xml")
-        assert coverage.containsElementWithAttrib("class", {"filename":"test/PlusTest.cpp"})
-        assert not coverage.containsElementWithAttrib("class", {"filename":"program/main.cpp"})
-        assert not coverage.containsElementWithAttrib("class", {"filename":"plus/src/plus.cpp"})
-        assert not coverage.containsElementWithAttrib("class", {"filename":"minius/src/minus.cpp"})
-        assert not coverage.containsElementWithAttrib("class", {"filename":"test/MinusTest.cpp"})
+        assert coverage["coverage/packages/package/classes/class"][0]["filename"] != "minus/src/minus.cpp"
+        assert coverage["coverage/packages/package/classes/class"][0]["filename"] != "plus/src/plus.cpp"
+        assert coverage["coverage/packages/package/classes/class"][0]["filename"] != "program/main.cpp"
+        assert coverage["coverage/packages/package/classes/class"][0]["filename"] != "test/MinusTest.cpp"
+        assert coverage["coverage/packages/package/classes/class"][0]["filename"] == "test/PlusTest.cpp"
 
 
 def test_autotools_project_coverage_filter(report_build):
     with report_build.files.conf() as conf:
         conf.set("SHIFT_COVERAGE_EXCLUDES", "program */src/*.cpp test/MinusTest.cpp")
         assert report_build.shell.execute("bitbake autotools-project -c coverage").stderr.empty()
-        
+
         coverage = report_build.files.readAsXml("report/autotools-project-1.0.0-r0/coverage/coverage.xml")
-        assert coverage.containsElementWithAttrib("class", {"filename":"test/PlusTest.cpp"})
-        assert not coverage.containsElementWithAttrib("class", {"filename":"program/main.cpp"})
-        assert not coverage.containsElementWithAttrib("class", {"filename":"plus/src/plus.cpp"})
-        assert not coverage.containsElementWithAttrib("class", {"filename":"minius/src/minus.cpp"})
-        assert not coverage.containsElementWithAttrib("class", {"filename":"test/MinusTest.cpp"})
+        assert coverage["coverage/packages/package/classes/class"]["filename"] != "minus/src/minus.cpp"
+        assert coverage["coverage/packages/package/classes/class"]["filename"] != "plus/src/plus.cpp"
+        assert coverage["coverage/packages/package/classes/class"]["filename"] != "program/main.cppd"
+        assert coverage["coverage/packages/package/classes/class"]["filename"] != "test/MinusTest.cpp"
+        assert coverage["coverage/packages/package/classes/class"]["filename"] == "test/PlusTest.cpp"
