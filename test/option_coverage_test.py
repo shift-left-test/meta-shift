63891246ee05912938923ed4433e34b4709f74f8
sangmo.kang <sangmo.kang@lge.com>
2022-07-13 16:56:58 +0900

    #658 Updates to duplo 0.8 / cppcheck 2.8 / cpplint 1.6

--- test/option_coverage_test.py
+++ test/option_coverage_test.py
@@ -10,12 +10,12 @@ import pytest
 
 def test_cmake_project_coverage_filter(report_build):
     with report_build.files.conf() as conf:
-        conf.set("SHIFT_COVERAGE_EXCLUDES", "program */src/*.cpp test/MinusTest.cpp")
+        conf.set("SHIFT_COVERAGE_EXCLUDES", "program *us/*.cpp test/MinusTest.cpp")
         assert report_build.shell.execute("bitbake cmake-project -c coverage").stderr.empty()
 
         coverage = report_build.files.readAsXml("report/cmake-project-1.0.0-r0/coverage/coverage.xml")
-        assert coverage["coverage/packages/package/classes/class"]["filename"] != "minus/src/minus.cpp"
-        assert coverage["coverage/packages/package/classes/class"]["filename"] != "plus/src/plus.cpp"
+        assert coverage["coverage/packages/package/classes/class"]["filename"] != "minus/minus.cpp"
+        assert coverage["coverage/packages/package/classes/class"]["filename"] != "plus/plus.cpp"
         assert coverage["coverage/packages/package/classes/class"]["filename"] != "program/main.cpp"
         assert coverage["coverage/packages/package/classes/class"]["filename"] != "test/MinusTest.cpp"
         assert coverage["coverage/packages/package/classes/class"]["filename"] == "test/PlusTest.cpp"
@@ -23,12 +23,12 @@ def test_cmake_project_coverage_filter(report_build):
 
 def test_qmake_project_coverage_filter(report_build):
     with report_build.files.conf() as conf:
-        conf.set("SHIFT_COVERAGE_EXCLUDES", "program */src/*.cpp test/MinusTest.cpp")
+        conf.set("SHIFT_COVERAGE_EXCLUDES", "program *us/*.cpp test/MinusTest.cpp")
         assert report_build.shell.execute("bitbake qmake-project -c coverage").stderr.empty()
 
         coverage = report_build.files.readAsXml("report/qmake-project-1.0.0-r0/coverage/coverage.xml")
-        assert coverage["coverage/packages/package/classes/class"][0]["filename"] != "minus/src/minus.cpp"
-        assert coverage["coverage/packages/package/classes/class"][0]["filename"] != "plus/src/plus.cpp"
+        assert coverage["coverage/packages/package/classes/class"][0]["filename"] != "minus/minus.cpp"
+        assert coverage["coverage/packages/package/classes/class"][0]["filename"] != "plus/plus.cpp"
         assert coverage["coverage/packages/package/classes/class"][0]["filename"] != "program/main.cpp"
         assert coverage["coverage/packages/package/classes/class"][0]["filename"] != "test/MinusTest.cpp"
         assert coverage["coverage/packages/package/classes/class"][0]["filename"] == "test/PlusTest.cpp"
@@ -36,12 +36,12 @@ def test_qmake_project_coverage_filter(report_build):
 
 def test_autotools_project_coverage_filter(report_build):
     with report_build.files.conf() as conf:
-        conf.set("SHIFT_COVERAGE_EXCLUDES", "program */src/*.cpp test/MinusTest.cpp")
+        conf.set("SHIFT_COVERAGE_EXCLUDES", "program *us/*.cpp test/MinusTest.cpp")
         assert report_build.shell.execute("bitbake autotools-project -c coverage").stderr.empty()
 
         coverage = report_build.files.readAsXml("report/autotools-project-1.0.0-r0/coverage/coverage.xml")
-        assert coverage["coverage/packages/package/classes/class"]["filename"] != "minus/src/minus.cpp"
-        assert coverage["coverage/packages/package/classes/class"]["filename"] != "plus/src/plus.cpp"
+        assert coverage["coverage/packages/package/classes/class"]["filename"] != "minus/minus.cpp"
+        assert coverage["coverage/packages/package/classes/class"]["filename"] != "plus/plus.cpp"
         assert coverage["coverage/packages/package/classes/class"]["filename"] != "program/main.cppd"
         assert coverage["coverage/packages/package/classes/class"]["filename"] != "test/MinusTest.cpp"
         assert coverage["coverage/packages/package/classes/class"]["filename"] == "test/PlusTest.cpp"
