f41063384435c7429d2424b49988e0ed24cea781
Sung Gon Kim <sunggon82.kim@lge.com>
2022-09-06 16:41:19 +0900

    #707 selftest: Add test cases

--- test/option_test_test.py
+++ test/option_test_test.py
@@ -36,3 +36,18 @@ def test_autotools_project_filter(test_build):
             "PlusTest.testShouldFail",
             "MinusTest.testShouldReturnExpectedValue",
             "MinusTest.testShouldAlsoFail")
+
+
+def test_do_test_shuffle_on(test_build):
+    with test_build.files.conf() as conf:
+        conf.set("SHIFT_TEST_SHUFFLE", "1")
+        first = test_build.shell.execute("bitbake sqlite3wrapper -c test").stdout
+        second = test_build.shell.execute("bitbake sqlite3wrapper -c test").stdout
+        assert first != second
+
+
+def test_do_test_suppress_failures_off(test_build):
+    with test_build.files.conf() as conf:
+        conf.set("SHIFT_TEST_SUPPRESS_FAILURES", "0")
+        o = test_build.shell.execute("bitbake cmake-project -c test")
+        assert o.returncode != 0
