7eb0a028e1ee8a61cc50b1e8ecd2a6a6b9f10799
Sung Gon Kim <sunggon82.kim@lge.com>
2025-05-02 02:31:14 +0900

    #778: do_coverage: Fix an issue that lcov fails to parse .gcno due to the -ffile-prefix-map option

--- test/conftest.py
+++ test/conftest.py
@@ -10,7 +10,7 @@ import pytest
 import shutil
 
 
-BRANCH = "master"
+BRANCH = "walnascar"
 
 
 @pytest.fixture(scope="session")
--- test/mini-mcf.py
+++ test/mini-mcf.py
@@ -18,7 +18,7 @@ import subprocess
 import tempfile
 
 
-BRANCH = "master"
+BRANCH = "walnascar"
 REPO_DIR = os.path.join(tempfile.gettempdir(), "meta-shift-repos-%s" % getpass.getuser())
 BUILD_DIR = "build"
 META_SHIFT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
