f976e893deac28bcd71e98477a90717f87b19926
Sung Gon Kim <sunggon82.kim@lge.com>
2026-01-05 11:29:44 +0900

    #787: Replace poky with bitbake and openembedded-core

--- test/bitbakelayers_inspect_test.py
+++ test/bitbakelayers_inspect_test.py
@@ -10,35 +10,30 @@ import pytest
 
 
 def test_default_format(release_build):
-    o = release_build.shell.execute("bitbake-layers inspect meta-poky")
+    o = release_build.shell.execute("bitbake-layers inspect meta")
     assert o.stdout.containsAll("General Information",
                                 "-------------------",
-                                "Layer: meta-poky",
-                                "Name: yocto",
+                                "Layer: meta",
+                                "Name: core",
                                 "Path:",
                                 "Conf:",
                                 "Priority: 5",
-                                "Version: 3",
                                 "Compatibilities:",
-                                "Dependencies: core",
+                                "Dependencies:",
                                 "Additional Information",
                                 "----------------------",
                                 "Images:",
-                                "Machines:",
-                                "Distros:",
-                                "Classes:")
+                                "Machines:")
 
 
 def test_json_format_save_as_file(release_build):
     with release_build.files.tempfile("report.json") as f:
-        o = release_build.shell.execute("bitbake-layers inspect meta-poky --output {}".format(f))
-        assert not o.stdout.contains('"Name": "yocto"')
+        o = release_build.shell.execute("bitbake-layers inspect meta --output {}".format(f))
+        assert not o.stdout.contains('"Name": "core"')
         with release_build.files.readAsJson("report.json") as data:
-            assert data["General Information"]["Name"] == "yocto"
-            assert data["General Information"]["Layer"] == "meta-poky"
+            assert data["General Information"]["Name"] == "core"
+            assert data["General Information"]["Layer"] == "meta"
             assert data["General Information"]["Priority"] == "5"
-            assert data["General Information"]["Version"] == "3"
-            assert data["General Information"]["Dependencies"] == "core"
 
 
 def test_inspect_unknown_layer(release_build):
